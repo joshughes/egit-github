7f6e3a80be9b8ae6a4b25e9fcf268c77b6b9563d
Joseph Hughes <joshughes@paypal.com>
2013-07-30 21:45:33 -0400

    Adding events to repo hook object

--- org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/RepositoryHook.java
+++ org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/RepositoryHook.java
@@ -12,6 +12,7 @@ package org.eclipse.egit.github.core;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.egit.github.core.util.DateUtils;
@@ -24,6 +25,81 @@ public class RepositoryHook implements Serializable {
 	/** serialVersionUID */
 	private static final long serialVersionUID = -9023469643749604324L;
 
+	/**
+	 *  Any git push to a Repository.
+	 */
+	public static final String PUSH = "push"; //$NON-NLS-1$
+
+	/**
+	 * Any time an Issue is opened or closed.
+	 */
+	public static final String ISSUES = "issues"; //$NON-NLS-1$
+
+	/**
+	 * Any time an Issue is commented on.
+	 */
+	public static final String ISSUE_COMMENT = "issue_comment"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Commit is commented on.
+	 */
+	public static final String COMMIT_COMMENT = "commit_comment"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Pull Request is opened, closed, or synchronized
+	 */
+	public static final String PULL_REQUEST = "pull_request"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Commit is commented on while inside a Pull Request review
+	 */
+	public static final String PULL_REQUEST_REVIEW_COMMENT = "pull_request_review_comment"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Wiki page is updated.
+	 */
+	public static final String GOLLUM = "gollum"; //$NON-NLS-1$
+
+	/**
+	 * Any time a User watches the Repository.
+	 */
+	public static final String WATCH = "watch"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Download is added to the Repository.
+	 */
+	public static final String DOWNLOAD = "download"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Download is added to the Repository.
+	 */
+	public static final String FORK = "fork"; //$NON-NLS-1$
+
+	/**
+	 * Any time a patch is applied to the Repository from the Fork Queue.
+	 */
+	public static final String FORK_APPLY = "fork_apply"; //$NON-NLS-1$
+
+	/**
+	 * Any time a User is added as a collaborator to a non-Organization Repository.
+	 */
+	public static final String MEMBER = "member"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Repository changes from private to public.
+	 */
+	public static final String PUBLIC = "public"; //$NON-NLS-1$
+
+	/**
+	 * Any time a team is added or modified on a Repository.
+	 */
+	public static final String TEAM_ADD = "team_add"; //$NON-NLS-1$
+
+	/**
+	 * Any time a Repository has a status update from the API
+	 */
+	public static final String STATUS = "status"; //$NON-NLS-1$
+
 	private boolean active;
 
 	private Date createdAt;
@@ -31,6 +107,8 @@ public class RepositoryHook implements Serializable {
 	private Date updatedAt;
 
 	private long id;
+
+	private List<String> events;
 
 	private RepositoryHookResponse lastResponse;
 
@@ -166,5 +244,20 @@ public class RepositoryHook implements Serializable {
 	public RepositoryHook setConfig(Map<String, String> config) {
 		this.config = config;
 		return this;
+	}
+
+	/**
+	 * @param events
+	 * @return this hook
+	 */
+	public List<String> getEvents() {
+		return events;
+	}
+
+	/**
+	 * @return events
+	 */
+	public void setEvents(List<String> events) {
+		this.events = events;
 	}
 }
